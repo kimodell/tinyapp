5d21231a70b3487441282b9719e28f0c394fdc09
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-29 14:40:03 -0500

    Add template for form page used to submit URLs. Add GET route to express_server.js to render template. Add POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -5,6 +5,12 @@ const PORT = 8080; // default port 8080
 //set EJS as view engine
 app.set("view engine", "ejs");
 
+//function to parse bodyfor POST from a Buffer to a string
+app.use(express.urlencoded({ extended: true }));
+
+//generate random string of 6 alphanumeric characters
+function generateRandomString() {}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,12 +38,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//route to render template in urls_new/.ejs in browser
+app.get("/urls/new", (req, res) =>{
+  res.render("urls_new");
+});
+
+
 //route handler to display single URL from urlDatabase and its shortened form (ID)
 app.get("/urls/:id", (req, res) => {
   //pass longURL and url ID to templateVars object
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }
   //pass templateVars containing single URL and it's shortened form to urls_show to diplay to client
   res.render("urls_show", templateVars);
+});
+
+
+//route to handle from submssion
+app.post("/urls", (req, res) => {
+  //logs POST request to the console
+  console.log(req.body);
+  res.send("Ok"); //respond with okay
 });
 
 app.listen(PORT, () => {
